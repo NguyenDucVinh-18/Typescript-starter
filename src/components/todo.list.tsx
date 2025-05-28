0cb437189c2559e579468949d69ec2f2919c36ae
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2025-05-28 18:28:22 +0700

    component todo list

---
+++ src/components/todo.list.tsx
@@ -0,0 +1,9 @@
+const TodoList = () =>{
+    return (
+        <div>
+            to do list
+        </div>
+    )
+}
+
+export default TodoList;
---
+++ src/layout.tsx
@@ -0,0 +1,15 @@
+import TodoList from "./components/todo.list"
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+       Hello world! This is a simple React app using TypeScript and Vite.
+      </p>
+      <TodoList />
+    </>
+  )
+}
+
+export default App
